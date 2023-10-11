251cc24673e437b231af7fbbc1d2d38840e9ef42
MartinezN2014 <2014nmartinez@gmail.com>
2023-10-11 14:28:26 -0400

    forgot to push what I had been working on

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// Imports
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use((req, res) => res.send("Wrong route!"));
+
+// Exports
+module.exports = router;
